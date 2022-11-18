dc174c86e62889a88ac95463e07beaba5b20d392
islom0906 <iabdugofurov99@gmail.com>
2022-11-18 22:36:23 +0500

    Islom's first components in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Javohir from "./Javohir/javohir";
 import Najmiddin from "./Najmiddin/Najmiddin";
+import Islom from './Islom/Islom'
 
 function App() {
   return (
     <>
       <Javohir />
       <Najmiddin />
+      <Islom name='Islom' job='Developer' />
     </>
   );
 }
