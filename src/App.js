e4ced131a99a0d52a8fcfde5203e15d73c45cfb4
islom0906 <iabdugofurov99@gmail.com>
2022-11-18 22:38:00 +0500

    Islom's first components in react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import "./App.css";
-
 import Javohir from "./Javohir/javohir";
 import Najmiddin from "./Najmiddin/Najmiddin";
-<<<<<<< HEAD
 import Islom from './Islom/Islom'
-=======
 import Elyor from "./Elyor/elyor";
->>>>>>> 9b46b50a813163056232fa0494554f32b400cd3d
+
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
+      <Elyor/>
       <Javohir />
       <Najmiddin />
       <Islom name='Islom' job='Developer' />
-=======
-      <Elyor/>
-      <Najmiddin/>
-      
-      <Javohir/>
-      
->>>>>>> 9b46b50a813163056232fa0494554f32b400cd3d
+
     </>
   );
 }
