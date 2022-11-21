d946b4a6981dd32159678fe5f741a8f28fdde716
icoderalbison <testalbison1103@gmail.com>
2022-11-21 15:03:01 +0500

    cleared

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-import Javohir from "./Javohir/javohir";
-import Najmiddin from "./Najmiddin/Najmiddin";
-import Islom from "./Islom/Islom";
-import Elyor from "./Elyor/elyor";
-import Nodir from "./Nodir/ownComponent";
-import Something from "./Nozimbek/Something";
-import Footer from "./IslombekNosirov/footer";
-<<<<<<< HEAD
-import Bunyodbek from "./Bunyodbek/bunyodbek";
-import { Provider } from "react-redux";
-import { store } from "./Gofur/store/store";
-import { GofursComponent } from "./Gofur/App";
-=======
-import Fazliddin from "./fazliddin/button";
-import { Provider } from "react-redux";
-import { store } from "./Gofur/store/store";
-import { GofursComponent } from "./Gofur/App";
-import Card from "./Abdulloh/Cards";
-import Quvondiq from "./Quvondiq/Quvondiq";
->>>>>>> 97e0a2235eec65e9b4c684956eef02349836cf05
+
 
 function App() {
   return (
     <>
-      <Fazliddin text="nmadr" />
-      <Elyor />
-      <Javohir />
-      <Najmiddin />
-      <Islom name="Islom" job="Developer" />
-      <Nodir name="Nodir" title="Web Dev" />
-      <Something />
-      <Footer />
-      <Bunyodbek />
-      <Provider store={store}>
-        <GofursComponent />
-      </Provider>
-      <Card />
-      <Quvondiq/>
+      cleared
     </>
   );
 }
--- src/Gofur/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from"@reduxjs/toolkit";
-import feedbacksSlice from "../components/feedbacks-slice/feedbacks-slice";
-
-
-export const store = configureStore({
-    reducer:{
-        feedbacks:feedbacksSlice,
-    }
-})
