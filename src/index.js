2788e8f091f486e4289bbce4494bc4476ce99ca9
erkiinov <erkinovbavrom12@gmail.com>
2022-11-23 15:32:57 +0500

    page1Section2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Section2 from "./component/page1/section2/Section2";
 import Sale from "./component/sale-component/Sale";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,6 +8,5 @@ root.render(
   <React.StrictMode>
     <App />
     <Sale />
-    <Section2/>
   </React.StrictMode>
 );
