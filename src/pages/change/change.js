e96f413021bc5a610ccaa24623197ec9ef89a095
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2022-11-23 17:54:38 +0500

    My message

--- src/pages/change/change.js
+++ src/pages/change/change.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Page2showcae1 from './page2_showcase1/P2sh1'
 
 const Change = () => {
   return (
-    <div>change</div>
+    <div>
+      <Page2showcae1/>
+
+    </div>
+    
   )
 }
 
