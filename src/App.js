74d96b22933d5b23d22ee36dfde52d6898a9828d
Sultvn <shavkatovs@yandex.com>
2022-11-24 02:48:57 +0500

    Delievery-types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Delivery from "./component/Delivery-section/Delivery";
 import Delievery_types from "./component/Varianti_dostavki/Varianti-dostavki";
 import Change from "./pages/change/change";
-import Change from "./pages/change/change";
 import ClientSection from "./component/ClientsSections/ClientSectionP";
 
 function App() {
