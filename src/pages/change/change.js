8d7910141677afff8912b91b7c7976ebc1eecccd
TheBakhodirov <91797611+TheBakhodirov@users.noreply.github.com>
2022-11-23 23:44:23 +0500

    added Homepage

--- src/pages/change/change.js
+++ src/pages/change/change.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Delivery from "../../component/Delivery-section/Delivery";
+import Section2 from "../../component/page1/section2/Section2";
+import Page2showcae1 from "../../component/page2_showcase1/P2sh1";
+import Sale from "../../component/sale-component/Sale";
+import Quvondiq from "../../component/HomeOtziv/Quvondiq";
+import Page1Showcase3 from "../../component/page1/page1Showcase3/Page1Showcase3";
 
 const Change = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Page1Showcase3 />
+      <Quvondiq />
+      <Delivery />
+      <Section2 />
+      <Page2showcae1 />
+      <Sale />
+    </div>
+  );
 };
 
 export default Change;
