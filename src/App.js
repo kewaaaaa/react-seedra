dfc4b20be855ce023425fc52828dd1795ace9c51
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2022-11-24 12:40:10 +0500

    added 2-page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Delievery_types from "./component/Varianti_dostavki/Varianti-dostavki";
 import Change from "./pages/change/change";
 import ClientSection from "./component/ClientsSections/ClientSectionP";
 import Homepage from "./pages/page1/page1";
+import Page1 from "./pages/page2/page2";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Delievery_types />
       <ClientSection /> */}
       <Change />
+      <hr />
+      <Page1 />
     </>
   );
 }
---
+++ src/pages/page2/page2.js
@@ -0,0 +1,14 @@
+import "./page2.scss";
+import Page2showcae1 from "../../component/page2_showcase1/P2sh1";
+import EasyToBuy from "../../component/page2_cards/EasyToBuy";
+
+const page2 = () => {
+  return (
+    <>
+      <Page2showcae1 />
+      <EasyToBuy />
+    </>
+  );
+};
+
+export default page2;
