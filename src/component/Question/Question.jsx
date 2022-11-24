6a11b84d340be6518be6af2cc3631b5c258e70a8
Abdujabbarof <abdujabborov6172@gmail.com>
2022-11-24 16:46:34 +0500

    added question c

---
+++ src/component/Question/Question.jsx
@@ -0,0 +1,16 @@
+function Question(){
+    return(
+        <section>
+            <div className="container">
+                <h1>Остались вопросы? Спросите нас в соцсетях!</h1>
+                <ul>
+                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-viber"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Question;
