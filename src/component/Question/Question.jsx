6b6b1fa712cec2c8286c24f1967156e283893d79
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2022-11-25 22:42:44 +0500

    Question fixed

--- src/component/Question/Question.jsx
+++ src/component/Question/Question.jsx
@@ -1,16 +1,30 @@
-function Question(){
-    return(
-        <section>
-            <div className="container">
-                <h1>Остались вопросы? Спросите нас в соцсетях!</h1>
-                <ul>
-                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-viber"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
-                </ul>
-            </div>
-        </section>
-    )
+import "./question.scss";
+
+function Question() {
+  return (
+    <section className="last-showcase">
+      <div className="inner-container">
+        <h1>Остались вопросы? Спросите нас в соцсетях!</h1>
+        <ul>
+          <li>
+            <a href="#">
+              <i class="fa-brands fa-whatsapp"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa-brands fa-viber"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa-brands fa-whatsapp"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Question;
