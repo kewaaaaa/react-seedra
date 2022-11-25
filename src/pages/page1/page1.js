55c457b9026250ee36c4cf8d0d8e5b864d82492c
TheBakhodirov <91797611+TheBakhodirov@users.noreply.github.com>
2022-11-25 19:38:42 +0500

    homepage is ready

--- src/pages/page1/page1.js
+++ src/pages/page1/page1.js
@@ -3,13 +3,10 @@ import "./page1.scss";
 import Showcase from "../../component/Delivery-section/Delivery";
 import Section2 from "../../component/page1/section2/Section2";
 import Delievery_types from "../../component/Varianti_dostavki/Varianti-dostavki";
+import Steps from "../../component/table-bottom/tablebottom";
 import Page1Showcase3 from "../../component/page1/page1Showcase3/Page1Showcase3";
+import ClientSection from "../../component/ClientsSections/ClientSectionP";
 import Feedbacks from "../../component/HomeOtziv/Quvondiq";
-/*
-  Components:
-  Finished = 4
-  Not finished = 2
-*/
 
 const Homepage = () => {
   return (
@@ -17,8 +14,9 @@ const Homepage = () => {
       <Showcase />
       <Section2 />
       <Delievery_types />
-
+      <Steps />
       <Page1Showcase3 />
+      <ClientSection />
       <Feedbacks />
     </div>
   );
