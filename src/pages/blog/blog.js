b8a43d219e5fb17ec27314800e5cb4ccaadc9ff1
Elyor <lutsiferdoom@gmail.com>
2022-11-27 22:23:50 +0500

    comment

---
+++ src/pages/blog/blog.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Question from '../../component/Question/Question'
+import Sale from '../../component/sale-component/Sale'
+
+const blog = () => {
+  return (
+    <div>
+      <Sale/>
+      
+        {/* card un joy */}
+      <Question/>
+    </div>
+  )
+}
+
+export default blog
