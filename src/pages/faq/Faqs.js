7dd424403723735bf1de1ad9bea0beac776d0a74
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2022-11-28 22:43:13 +0500

    FAQ initialized

---
+++ src/pages/faq/Faqs.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Chasto from "../../component/Chasto/Chasto.js";
+import Question from "../../component/Question/Question.jsx";
+
+class Faqs extends Component {
+  render() {
+    return (
+      <>
+        <Chasto />
+        <Question />
+      </>
+    );
+  }
+}
+
+export default Faqs;
