cf430ed21a0dc9b1d1a9dfc1ff2a1fb34ccab851
Bunyodbek <bunyodbek.ho@gmail.com>
2022-11-28 22:27:01 +0500

    added card block to blog page

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Question from '../../component/Question/Question'
-import Sale from '../../component/sale-component/Sale'
-import Cardblog from './cardblog'
-import "./blog.scss"
+import React from "react";
+import Question from "../../component/Question/Question";
+import Sale from "../../component/sale-component/Sale";
+import Cardblog from "./cardblog";
+import "./blog.scss";
 
+import BlogCard from "../../component/blogCard/BlogCard";
 
 const blog = () => {
   return (
-    <div className='container'>
-      <Sale/>
+    <div className="container">
+      <Sale />
       {/* bu map cardlar un edi */}
       {/* <div className='cardlar-blog'>
           {
@@ -18,14 +19,11 @@ const blog = () => {
           }
       </div> */}
       {/* bu map cardlar un edi  end */}
-         
-        
-         
-        
-      {/* card un joy */}
-      <Question/>
-    </div>
-  )
-}
 
-export default blog
+      {/* card un joy */}
+      <Question />
+    </div>
+  );
+};
+
+export default blog;
