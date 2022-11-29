2fed7e7949ecea846649ebc540848753880120a0
javohir12312 <abduxalilovjavohir393@gmail.com>
2022-11-29 10:24:31 +0300

    add hovers

--- src/component/prices-section-page-4/Price.jsx
+++ src/component/prices-section-page-4/Price.jsx
@@ -6,57 +6,37 @@ function Price() {
     let data1 = data
     console.log(data1);
     return (
-        <div className='price_box'>
-            <p>
-                <a href="#">
-                    Главная
-                </a>
-                /
-                <a href="#">
-                    Цены
-                </a>
+        <>
+            <div className='price_box'>
+                <p>
+                    <a href="#">
+                        Главная
+                    </a>
+                    /
+                    <a href="#">
+                        Цены
+                    </a>
 
-            </p>
-            <h3>Услуги доставки из Великобритании</h3>
+                </p>
+                <h3>Услуги доставки из Великобритании</h3>
+            </div>
 
-            <ul className='list'>
-                {data1.map((index) => {
-                    return (
-                        <li key={index.id}>
-                            <img src={index.img} alt="" />
-                            <h4>{index.title}</h4>
-                            <p>{index.description}</p>
-                            <button>
-                                {index.btn}
-                            </button>
-                        </li>)
-                })}
-                {/* <li>
-                    <img src="" alt="" />
-                    <h4></h4>
-                    <p></p>
-                    <button></button>
-                </li>
-                <li>
-                    <img src="" alt="" />
-                    <h4></h4>
-                    <p></p>
-                    <button></button>
-                </li>
-                <li>
-                    <img src="" alt="" />
-                    <h4></h4>
-                    <p></p>
-                    <button></button>
-                </li>
-                <li>
-                    <img src="" alt="" />
-                    <h4></h4>
-                    <p></p>
-                    <button></button>
-                </li> */}
-            </ul>
-        </div>
+            <div>
+                <ul className='list'>
+                    {data1.map((index) => {
+                        return (
+                            <li key={index.id}>
+                                <img src={index.img} alt="" />
+                                <h4>{index.title}</h4>
+                                <p>{index.description}</p>
+                                <button>
+                                    {index.btn}
+                                </button>
+                            </li>)
+                    })}
+                </ul>
+            </div>
+        </>
     )
 }
 
