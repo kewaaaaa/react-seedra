e7899f03603e3f032a8ec88b9de05e05247bf151
islom0906 <iabdugofurov99@gmail.com>
2022-11-29 13:02:08 +0500

    for pull

--- src/pages/price/price.js
+++ src/pages/price/price.js
@@ -3,14 +3,21 @@ import React from 'react'
 import PriceSectionPage4 from '../../component/prices-section-page-4/Price'
 import OnlineCalc from '../../component/page3_online-calc/OnlineCalc'
 import Uslugi from '../../component/uslugi_sklada/Uslugi'
-
+import VariantDostavki from '../../component/Varianti_dostavki/Varianti-dostavki'
+import Question from "../../component/Question/Question";
+import './price.scss'
 
 const Price = () => {
   return (
-    <div>
-      <PriceSectionPage4/>
+    <div className='price'>
+      <div className='price__section'>
+      <PriceSectionPage4  />
+        
+      </div>
       <OnlineCalc/>
+      <VariantDostavki/>
       <Uslugi/>
+      <Question />
     </div>
   )
 }
