1e6637337507bc23d408b8c23d8a7c96c2367143
islom0906 <iabdugofurov99@gmail.com>
2022-11-30 19:40:48 +0500

    for pull

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Cards from '../../component/cards/Cards'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Cards BigTitle="Доставка товаров из Великобритании в Россию" text='Начните экономить до 80% на шопинге. Регистрируйтесь в OkeyPost, чтобы покупать со скидками в UK одежду, обувь, гаджеты известных брендов и безопасно отправлять вещи в Россию.'
+      button={'Получить адрес в UK'}
+      imgRight={'./assets/images/page1/page1_showcase1.png'}/>
+    </div>
   )
 }
 
