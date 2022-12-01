17830b42e1a81a97d408695b9f19de375634a2bb
madaminov.muhammadjon1 <madaminov.muhammadjon1@gmail.com>
2022-12-01 15:38:17 +0500

    change icon

--- src/component/accordion/Accordion.js
+++ src/component/accordion/Accordion.js
@@ -9,23 +9,24 @@ function Accordion(props) {
   function plus(target) {
     setState(1);
 
+    target.style.transform = "rotate(0)";
     answer.current.style.display = "block";
     setTimeout(() => {
       answer.current.style.transform = "translate(0, 0)";
     }, 0);
 
-    target.style.transform = "rotate(135deg)";
   }
   function minus(target) {
     setState(0);
 
+    target.style.transform = "rotate(180deg)";
     answer.current.style.transform = "translate(0, -20px)";
+    answer.current.style.transition = "2s";
 
     setTimeout(() => {
       answer.current.style.display = "none";
     }, 200);
 
-    target.style.transform = "rotate(0)";
   }
   function click({ target }) {
     state ? minus(target) : plus(target);
@@ -33,21 +34,21 @@ function Accordion(props) {
 
   return (
     <>
-    <div className="accor">
-      <div className="container">
-      <h1 className={accor}>{props.title}</h1>
-      <div className="accor__card">
-        <div className="accor__quest">{props.question}</div>
-        <button onClick={click} className="accor__arrow">
-          +
-        </button>
-        <div className="accor__ansDiv">
-          <div className="accor__ans" ref={answer}>
-            {props.answer}
+      <div className="accor">
+        <div className="container">
+          <h1 className={accor}>{props.title}</h1>
+          <div className="accor__card">
+            <div className="accor__quest">{props.question}</div>
+            <button onClick={click} className="accor__arrow">
+              ^
+            </button>
+            <div className="accor__ansDiv">
+              <div className="accor__ans" ref={answer}>
+                {props.answer}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {console.log("render")}
+          {console.log("render")}
         </div>
       </div>
     </>
