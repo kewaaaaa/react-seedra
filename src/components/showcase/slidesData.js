4af9ad91edcf7705a4307d38546b34ad120faf02
TheBakhodirov <91797611+TheBakhodirov@users.noreply.github.com>
2022-12-04 00:30:22 +0500

    added showcase component|homepage

---
+++ src/components/showcase/slidesData.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    img: "./assets/media/showcase/slide1.png",
+    title: "Inspiring adventure and confidence",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit placerat sit purus molestie.",
+    button: "Apply",
+  },
+  {
+    img: "./assets/media/showcase/slide1.png",
+    title: "Inspiring adventure and confidence",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit placerat sit purus molestie.",
+    button: "Apply",
+  },
+  {
+    img: "./assets/media/showcase/slide1.png",
+    title: "Inspiring adventure and confidence",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit placerat sit purus molestie.",
+    button: "Apply",
+  },
+  {
+    img: "./assets/media/showcase/slide1.png",
+    title: "Inspiring adventure and confidence",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit placerat sit purus molestie.",
+    button: "Apply",
+  },
+];
