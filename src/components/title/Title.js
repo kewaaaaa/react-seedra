141a0c7d9017966795099f906b956682c50a46ce
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2022-12-06 00:02:16 +0500

    title fixed main layout ready

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,14 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import "./style.scss";
 
 function Title(props) {
-  if(props.title){
-    return <h1 id='title'>{props.title}</h1>
-  } else if (props.titleUp){
-    return <h1 id='titleUp'>{props.titleUp}</h1>
-  } else if (props.titleDown){
-    return <h1 id='titleDown'>{props.titleDown}</h1>
+  if (props.title) {
+    return <h1 id="title">{props.title}</h1>;
+  } else if (props.titleUp) {
+    return <h1 id="titleUp">{props.titleUp}</h1>;
+  } else if (props.titleDown) {
+    return <h1 id="titleDown">{props.titleDown}</h1>;
   }
 }
 
-export default Title
+export default Title;
