9668029bba167abaec94d0f5815c3e2261ed3686
erkiinov <erkinovbavrom12@gmail.com>
2022-12-05 21:14:34 +0500

    autoCarusel

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import MainLayout from "./layouts/about/Main.layout";
 import Tabs from "./components/tab/Tabs";
 import data2 from './components/tab/tabsType2.json'
 import data1 from './components/tab/tabsType1.json'
+import Carousel, { CarouselItem } from "./components/CoruselPartner/Corusel";
 
 function App() {
   return (
     <div className="App">
-      <MainLayout>
+      {/* <MainLayout>
       <Tabs {...data1}/>
-
-      </MainLayout>
+      
+      </MainLayout> */}
+      <Carousel>
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+        <CarouselItem img={123} />
+      </Carousel>
     </div>
   );
 }
--- src/components/CoruselPartner/Corusel.js
+++ src/components/CoruselPartner/Corusel.js
@@ -3,12 +3,16 @@ import '../title/style.scss'
 
 
 
-export const CarouselItem = ({children, width}) => {
-  return (
-    <div className='carousel-item' style={{width: width}}>
-        {children}
-    </div>
-  )
+export const CarouselItem = (props) => {
+    if(props.url && props.img){
+        return (
+            <div className='carousel-item'><a href={props.url}>{props.img}</a></div>
+        )
+    } else if (props.img){
+        return (
+            <div className='carousel-item'><a href='/'>{props.img}</a></div>
+        )
+    } 
 };
 
 
@@ -23,17 +27,16 @@ const Carousel = ({children}) => {
     const updateIndex = (newIndex) => {
         if(newIndex < 0){
             newIndex = React.Children.count(children) - 1/5
-        } else if(newIndex *3.2 +1 >= React.Children.count(children)){
+        } else if(newIndex *7 >= React.Children.count(children)){
             newIndex = 0
         }
-        console.log(`${newIndex *3.2 + 1}-newIndex`);
         setActiveIndex(newIndex)
     }
 
     useEffect(()=>{
         const interval = setInterval(()=>{
             if (!paused){
-                updateIndex(activeIndex + 1/5)
+                updateIndex(activeIndex + 1/4)
             }
         }, 1000)
 
@@ -53,9 +56,8 @@ const Carousel = ({children}) => {
         onMouseLeave={()=> setPaused(false)}
     >
         <div className='inner' style={{transform: `translateX(-${activeIndex * 750}px)`}}>
-        {console.log(activeIndex)}
             {React.Children.map(children, (child, index) => {
-                return React.cloneElement(child, {width: '360px'})
+                return React.cloneElement(child, {width: '300px'})
             })}
 
         </div>
