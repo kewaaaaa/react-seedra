0830ddabe3b0296e3a42892a8170c325bce702d0
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2022-12-06 03:01:02 +0500

    CareerPage

---
+++ src/pages/careerPage/CareerPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CurrentPage from "../../components/CurrentPage/CurrentPage";
+import Title from "../../components/title/Title";
+import Strick from "../../components/title/Stick";
+import Tabs from "../../components/tab/Tabs";
+import data from "../../components/tab/tabsType2.json";
+import "./careerStyle.scss";
+
+function CareerPage() {
+  return (
+    <div className="carCent">
+      <CurrentPage />
+      <Title title="Career Centre" />
+      <div className="carStick">
+        <Strick color="#FFC600" />
+      </div>
+      <Tabs {...data} />
+    </div>
+  );
+}
+
+export default CareerPage;
