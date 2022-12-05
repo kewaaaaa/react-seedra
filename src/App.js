b12d178581280b98c35d4fbd55b5d7cadb44600d
erkiinov <erkinovbavrom12@gmail.com>
2022-12-06 00:20:59 +0500

    Title

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/about/Main.layout";
 import Tabs from "./components/tab/Tabs";
 import data1 from './components/tab/tabsType1.json'
-import Title from "./components/title/Title";
 
 function App() {
   return (
     <div className="App">
-      {/* <MainLayout>
+      <MainLayout>
       <Tabs {...data1}/>
 
-      </MainLayout> */}
-      <Title titleUpLink={'123 123 123'}/>
+      </MainLayout>
     </div>
   );
 }
