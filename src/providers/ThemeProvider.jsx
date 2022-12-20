a564b06221973ace87986084b000fdfbe5c68171
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2022-12-20 23:41:36 +0500

    dark mode

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -2,6 +2,7 @@ import React, { useState, createContext } from "react";
 export const ThemeContext = createContext({ type: "Light" });
 export const ThemeProvider = ({ children }) => {
   const [type, setType] = useState('Dark');
+  
   return (
     <ThemeContext.Provider value={{ type, setType }}>
       {children}
